b9af3097423905efb30da7fede3dede1cff34602
Mayank Rusia <mayankrusia97@gmail.com>
2023-05-24 12:30:37 +0530

    new file fact.java added to main

---
+++ fact.java
@@ -0,0 +1,12 @@
+public class fact {
+    public static void main(String[] args) 
+    {int f=1;
+        int x=5;
+        for(int i = x; i>=1;i--)
+        {
+            f = f*i;
+        }
+        System.out.println("Factorial of"+x+" is :"+f);
+    }
+    
+}
